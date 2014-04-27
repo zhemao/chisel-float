e271ce1fe37ce38a35f3986934462c8b14f14f1b
Howard Mao <zhehao.mao@gmail.com>
2014-04-26 22:24:48 -0700

    change handling of denormalized numbers

--- src/main/scala/FloatWrapper.scala
+++ src/main/scala/FloatWrapper.scala
@@ -9,11 +9,17 @@ class FloatWrapper(val num: Bits) {
     val (sign, exponent, mantissa, zero) = num.width match {
         case 32 => (num(31).toBool(),
                     num(30, 23).toUInt(),
-                    Cat(Bits(1, 1), num(22, 0).toUInt()),
-                    num(30, 0).toUInt() === UInt(0))
+                    // if the exponent is 0
+                    // this is a denormalized number
+                    Cat(Mux(num(30, 23) === Bits(0),
+                            Bits(0, 1), Bits(1, 1)),
+                        num(22, 0).toUInt()),
+                    num(30, 0) === Bits(0))
         case 64 => (num(63).toBool(),
                     num(62, 52).toUInt(),
-                    Cat(Bits(1, 1), num(51, 0).toUInt()),
-                    num(62, 0).toUInt() === UInt(0))
+                    Cat(Mux(num(62, 52) === Bits(0),
+                            Bits(0, 1), Bits(1, 1)),
+                        num(51, 0).toUInt()),
+                    num(62, 0) === Bits(0))
     }
 }
